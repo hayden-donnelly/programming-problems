2439ff9a89c813567a04e5b537550c260f8f528c
Hayden Donnelly <Bizbud@live.com>
2021-02-11 19:10:09 -0500

    ..

--- pretty_average_primes/pretty_average_primes.c
+++ pretty_average_primes/pretty_average_primes.c
@@ -3,52 +3,34 @@
 void find_pairs(int number)
 {
     int goal = number*2;
-    int a = 2;
-    int b = 2;
+    int a = 3;
+    int b = 0;
 
     for(int i = 2; i <= a; i++)
     {
-        if(a % i == 0 && i != a)
+        if(i == a)
         {
-            a++;
-            i = 1;
-            continue;
-        }
-        else if(i == a)
-        {
-            if(a > number)
-            {
-                break;
-            }
-            else
-            {
-                b = a;
-                for(int k = 2; a+b <= goal; k++)
+            b = goal - a;
+            for(int k = 2; k <= b; k++)
+            { 
+                if(k == b)
                 {
-                    if(b % k == 0 && k != b)
-                    {
-                        b++;
-                        k = 1;
-                        continue;
-                    }
-                    else if(k == b)
-                    {
-                        if(a+b == goal)
-                        {
-                            printf("%d %d\n", a, b);
-                            return;
-                        }
-                        else
-                        {
-                            b++;
-                            k = 1;
-                        }
-                    }
+                    printf("%d %d\n", a, b);
+                    return;
                 }
+                else if(b % k == 0)
+                {
+                    a++;
+                    i = 1;
+                    break;
+                }  
             }
+        }
+        else if(a % i == 0)
+        {
             a++;
             i = 1;
-        }
+        }   
     }
 }
 
