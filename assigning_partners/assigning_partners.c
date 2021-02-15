eef58645e2dab79703f71743c52295a5af6e39e1
Hayden Donnelly <Bizbud@live.com>
2021-02-15 10:45:07 -0500

    ..

--- assigning_partners/assigning_partners.c
+++ assigning_partners/assigning_partners.c
@@ -4,22 +4,37 @@
 
 int main()
 {
+    struct pair
+    {
+        char first_name[20];
+        char second_name[20];
+    };
+
     int n = 0;
     scanf("%d", &n);
-    char **names = (char**)malloc(sizeof(char*)*n); 
+
+    struct pair **names = (struct pair**)malloc(sizeof(struct pair*)*n); 
     for(int i = 0; i < n; i++)
     {
-        names[i] = malloc(sizeof(char)*20);
-        scanf("%s", names[i]);
+        names[i] = (struct pair*)malloc(sizeof(struct pair)*20);
+        scanf("%s", names[i]->first_name);
     }
     for(int i = 0; i < n; i++)
     {
-        char *current_name = malloc(sizeof(char)*20);
-        scanf("%s", current_name);
-        if(strcmp(names[n-1-i], current_name) != 0 || strcmp(names[i], current_name) == 0)
+        scanf("%s", names[i]->second_name);
+        if(strcmp(names[i]->first_name, names[i]->second_name) == 0)
         {
             printf("bad\n");
             return 0;
+        }
+        for(int k = i-1; k > -1; k--)
+        {
+            if(strcmp(names[i]->second_name, names[k]->first_name) == 0
+                && strcmp(names[i]->first_name, names[k]->second_name) != 0)
+            {
+                printf("bad\n");
+                return 0;
+            }
         }
     }
     printf("good\n");
