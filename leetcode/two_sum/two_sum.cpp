b1d1df5c8751dae7b0c35744587c6b0c8f5bfbf3
hayden-donnelly <donnellyhd@outlook.com>
2022-01-24 19:06:42 -0500

    solved first leetcode problem with cpp

--- leetcode/two_sum/two_sum.cpp
+++ leetcode/two_sum/two_sum.cpp
@@ -1,9 +1,30 @@
-#include <vector.hpp>
+#include <vector>
+
+using namespace std;
 
 class Solution
 {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        for(int x = 0; x < nums.size(); x++)
+        {
+            for(int y = 0; y < nums.size(); y++)
+            {
+                if(x == y)
+                {
+                    continue;
+                }
+                if(nums[x] + nums[y] == target)
+                {
+                    vector<int> solution {x, y};
+                    return solution;
+                }
+            }
+        }
+        
+        // This should never be run because there should always be a solution.
+        vector<int> solution {0, 0};
+        return solution;
     }
 };
