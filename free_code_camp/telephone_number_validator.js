68a1736da7947c66e898313ed43cc08d46db9bc7
Hayden Donnelly <30982485+hayden-donnelly@users.noreply.github.com>
2022-02-27 16:59:23 -0500

    Update telephone_number_validator.js

--- free_code_camp/telephone_number_validator.js
+++ free_code_camp/telephone_number_validator.js
@@ -1,15 +1,15 @@
 function telephoneCheck(str) {
-  if(/[0-9]{3}-[0-9]{3}-[0-9]{4}/.test(str)) {
-    return true;
-  } else if (/\([0-9]{3}\)[0-9]{3}-[0-9]{4}/.test(str)) {
-    return true;
-  } else if (/\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}/.test(str)) {
-    return true;
-  } else if(/[0-9]{3}\s[0-9]{3}\s[0-9]{4}/.test(str)) {
-    return true;
-  } else if(/[0-9]{10}/.test(str)) {
-    return true;
-  } else if(/1\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}/.test(str)) {
+  if( 
+  // 555-555-5555
+  /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(str)
+  // (555)555-5555
+  || /^(\([0-9]{3}\))[0-9]{3}-[0-9]{4}$/.test(str)
+  // (555) 555-5555
+  || /^(\([0-9]{3}\))\s[0-9]{3}-[0-9]{4}$/.test(str)
+   // 555 555 5555
+  || /^(1)\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}$/.test(str)
+  // 1 555 555 5555
+  || /^[0-9]{10}$/.test(str)) {
     return true;
   }
   return false;
