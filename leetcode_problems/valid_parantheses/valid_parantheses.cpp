68ef6e85e3f96fc543627fc270b49ef2acc50971
hayden-donnelly <donnellyhd@outlook.com>
2022-07-19 17:17:00 -0500

    Added valid parantheses.

---
+++ leetcode_problems/valid_parantheses/valid_parantheses.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    bool isValid(string s) 
+    {
+        stack<char> openingParantheses;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                openingParantheses.push(s[i]);
+            }
+            else
+            {
+                if(openingParantheses.empty())
+                {
+                    return false;
+                }
+                char currentParanthese = openingParantheses.top();
+                openingParantheses.pop();
+                if(currentParanthese == '(' && s[i] == ')'
+                    || currentParanthese == '{' && s[i] == '}'
+                    || currentParanthese == '[' && s[i] == ']')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return openingParantheses.empty();
+    }
+};
