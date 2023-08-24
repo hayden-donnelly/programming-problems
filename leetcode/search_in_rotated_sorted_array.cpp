fd1260179e9b0c61239b80c823a7a6973080f864
Hayden Donnelly <donnellyhd@outlook.com>
2023-08-24 12:48:11 -0400

    Update search_in_rotated_sorted_array.

--- leetcode/search_in_rotated_sorted_array.cpp
+++ leetcode/search_in_rotated_sorted_array.cpp
@@ -3,16 +3,6 @@
 class Solution 
 {
 public:
-    int undo_pivot(int i, int pivot, int size)
-    {
-        i = i - pivot;
-        if(i < 0)
-        {
-            i = size + i;
-        }
-        return i;
-    }
-
     int search(vector<int>& nums, int target) 
     {
         int l = 0;
@@ -21,37 +11,35 @@ public:
         while(l <= r)
         {
             m = l + (r - l) / 2;
-            if(nums[m] > nums[r])
+            if(nums[m] == target)
             {
-                l = m + 1;
+                return m;
             }
-            else if(nums[m] < nums[r])
+            if(nums[l] <= nums[m])
             {
-                r = m - 1;
-            }
-        }
-
-        l = 0;
-        r = nums.size()-1;
-        int pivot = m;
-        int m_offset = 0;
-        while(l <= r)
-        {
-            //r_offset = undo_pivot(r, pivot, nums.size());
-            //l_offset = undo_pivot(l, pivot, nums.size());
-            m = l + (r - l) / 2;
-            m_offset = undo_pivot(m, pivot, nums.size());
-            if(nums[m_offset] < target)
-            {
-                r = m - 1;
-            }
-            else if(nums[m_offset] > target)
-            {
-                l = m + 1;
+                // left is sorted
+                if(nums[l] <= target && target < nums[m])
+                {
+                    // target is between l and m
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
             }
             else
             {
-                return m_offset;
+                // right is sorted
+                if(nums[r] >= target && target > nums[m])
+                {
+                    // target is between m and r
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
             }
         }
         return -1;
